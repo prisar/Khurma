eb7f996cd8e9c6e98023d41a571d692dee765f02
prisar <pritimysarkar2010@gmail.com>
2017-12-19 15:26:13 +0530

    Lesson 4: StreetView grand canyon

--- app/src/main/java/com/example/prisar/khurma/MapActivity.java
+++ app/src/main/java/com/example/prisar/khurma/MapActivity.java
@@ -9,6 +9,9 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
+import com.google.android.gms.maps.StreetViewPanorama;
+import com.google.android.gms.maps.StreetViewPanoramaFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.CircleOptions;
@@ -16,93 +19,33 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.maps.model.StreetViewPanoramaCamera;
 
-public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MapActivity extends AppCompatActivity implements OnStreetViewPanoramaReadyCallback {
 
-    private GoogleMap mMap;
-    private boolean mapReady = false;
-
-    static final LatLng newYork = new LatLng(40.7484, -73.9857);
-    static final LatLng seattle = new LatLng(47.6204, -122.3491);
-    static final LatLng tokyo = new LatLng(40.7127, 74.0059);
-    static final LatLng dublin = new LatLng(53.3478, 6.2597);
-
-    static final CameraPosition NEWYORK = CameraPosition.builder()
-            .target(newYork)
-            .zoom(21)
-            .bearing(0)
-            .tilt(45)
-            .build();
-
-    static final CameraPosition SEATTLE = CameraPosition.builder()
-            .target(seattle)
-            .zoom(17)
-            .bearing(0)
-            .tilt(45)
-            .build();
-
-    static final CameraPosition TOKYO = CameraPosition.builder()
-            .target(tokyo)
-            .zoom(17)
-            .bearing(90)
-            .tilt(45)
-            .build();
-
-    static final CameraPosition DUBLIN = CameraPosition.builder()
-            .target(dublin)
-            .zoom(17)
-            .bearing(90)
-            .tilt(45)
-            .build();
-
-
-    LatLng renton;
-    LatLng kirland;
-    LatLng everett;
-    LatLng lynnwood;
-    LatLng montlake;
-    LatLng kent;
-    LatLng showare;
+    LatLng target = new LatLng(36.0579667, -112.1430996);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-        renton = new LatLng(47.489805, -122.120502);
-
-        kirland = new LatLng(47.7301986, -122.1768858);
-
-        everett = new LatLng(47.978748, -122.202001);
-
-        lynnwood = new LatLng(47.819533, -122.32288);
-
-        montlake = new LatLng(47.7973733, -122.3281771);
-
-        kent = new LatLng(47.385938, -122.258212);
-
-        showare = new LatLng(47.38702, -122.23986);
-
-        MapFragment mapFragment = (MapFragment) getFragmentManager()
-                .findFragmentById(R.id.mapOutput);
-        mapFragment.getMapAsync(this);
+        StreetViewPanoramaFragment streetViewPanoramaFragment = (StreetViewPanoramaFragment) getFragmentManager()
+                .findFragmentById(R.id.streetviewpanorama);
+        streetViewPanoramaFragment.getStreetViewPanoramaAsync(this);
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mapReady = true;
-        mMap = googleMap;
-        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        flyTo(SEATTLE);
+    public void onStreetViewPanoramaReady(StreetViewPanorama streetViewPanorama) {
+        streetViewPanorama.setPosition(target);
 
-        mMap.addCircle(new CircleOptions()
-        .center(renton)
-        .radius(1000)
-        .fillColor(Color.argb(64, 0,255, 0))
-        .strokeColor(Color.GREEN));
+        StreetViewPanoramaCamera streetViewPanoramaCamera = StreetViewPanoramaCamera.builder()
+                .zoom(14)
+                .bearing(40)
+                .tilt(0)
+                .build();
+
+        streetViewPanorama.animateTo(streetViewPanoramaCamera, 20000);
     }
 
-    public void flyTo(CameraPosition target) {
-        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(target), 10000, null);
-    }
 }
