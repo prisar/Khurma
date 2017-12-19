2b820d8fabe86b9a423d577fc699c1d25174174c
prisar <pritimysarkar2010@gmail.com>
2017-12-19 13:27:17 +0530

    Make the camera move: update

--- app/src/main/java/com/example/prisar/khurma/MapActivity.java
+++ app/src/main/java/com/example/prisar/khurma/MapActivity.java
@@ -16,6 +16,39 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     private GoogleMap mMap;
     private boolean mapReady = false;
 
+    static final LatLng newYork = new LatLng(40.7484, -73.9857);
+    static final LatLng seattle = new LatLng(47.6204, -122.3491);
+    static final LatLng tokyo = new LatLng(40.7127, 74.0059);
+    static final LatLng dublin = new LatLng(53.3478, 6.2597);
+
+    static final CameraPosition NEWYORK = CameraPosition.builder()
+            .target(newYork)
+            .zoom(21)
+            .bearing(0)
+            .tilt(45)
+            .build();
+
+    static final CameraPosition SEATTLE = CameraPosition.builder()
+            .target(seattle)
+            .zoom(17)
+            .bearing(0)
+            .tilt(45)
+            .build();
+
+    static final CameraPosition TOKYO = CameraPosition.builder()
+            .target(tokyo)
+            .zoom(17)
+            .bearing(90)
+            .tilt(45)
+            .build();
+
+    static final CameraPosition DUBLIN = CameraPosition.builder()
+            .target(dublin)
+            .zoom(17)
+            .bearing(90)
+            .tilt(45)
+            .build();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,29 +56,20 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
         Button btnSeattle = (Button) findViewById(R.id.btnSeattle);
         btnSeattle.setOnClickListener((v) -> {
-            if(mapReady){
-                LatLng seattle = new LatLng(47.6204, -122.3491);
-                CameraPosition cp = CameraPosition.builder().target(seattle).zoom(14).bearing((float) 112.5).tilt(65).build();
-                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cp), 5000, null);
-            }
+            if(mapReady)
+                flyTo(SEATTLE);
         });
 
         Button btnTokyo = (Button) findViewById(R.id.btnTokyo);
         btnTokyo.setOnClickListener((v) -> {
-            if(mapReady) {
-                LatLng tokyo = new LatLng(40.7127, 74.0059);
-                CameraPosition cp = CameraPosition.builder().target(tokyo).zoom(14).bearing((float) 112.5).tilt(65).build();
-                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cp), 5000, null);
-            }
+            if(mapReady)
+                flyTo(TOKYO);
         });
 
         Button btnDublin = (Button) findViewById(R.id.btnDublin);
         btnDublin.setOnClickListener((v) -> {
-            if(mapReady){
-                LatLng dublin = new LatLng(53.3478, 6.2597);
-                CameraPosition cp = CameraPosition.builder().target(dublin).zoom(14).bearing((float) 112.5).tilt(65).build();
-                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cp), 5000, null);
-            }
+            if(mapReady)
+                flyTo(DUBLIN);
         });
 
         MapFragment mapFragment = (MapFragment) getFragmentManager()
@@ -57,9 +81,11 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mapReady = true;
         mMap = googleMap;
-        LatLng newYork = new LatLng(40.7484, -73.9857);
-        CameraPosition target = CameraPosition.builder().target(newYork).zoom(17).bearing((float) 112.5).tilt(65).build();
-        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(target));
         mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        flyTo(NEWYORK);
+    }
+
+    public void flyTo(CameraPosition target) {
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(target), 10000, null);
     }
 }
