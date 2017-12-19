bb71d7eca61c13d755a20271eb0d9aedb4e0a097
prisar <pritimysarkar2010@gmail.com>
2017-12-19 13:10:51 +0530

    Make the camera move

--- app/src/main/java/com/example/prisar/khurma/MapActivity.java
+++ app/src/main/java/com/example/prisar/khurma/MapActivity.java
@@ -21,22 +21,31 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-        Button btnMap = (Button) findViewById(R.id.btnMap);
-        btnMap.setOnClickListener((v) -> {
-            if(mapReady)
-                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        Button btnSeattle = (Button) findViewById(R.id.btnSeattle);
+        btnSeattle.setOnClickListener((v) -> {
+            if(mapReady){
+                LatLng seattle = new LatLng(47.6204, -122.3491);
+                CameraPosition cp = CameraPosition.builder().target(seattle).zoom(14).bearing((float) 112.5).tilt(65).build();
+                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cp), 5000, null);
+            }
         });
 
-        Button btnSatellite = (Button) findViewById(R.id.btnSatellite);
-        btnSatellite.setOnClickListener((v) -> {
-            if(mapReady)
-                mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        Button btnTokyo = (Button) findViewById(R.id.btnTokyo);
+        btnTokyo.setOnClickListener((v) -> {
+            if(mapReady) {
+                LatLng tokyo = new LatLng(40.7127, 74.0059);
+                CameraPosition cp = CameraPosition.builder().target(tokyo).zoom(14).bearing((float) 112.5).tilt(65).build();
+                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cp), 5000, null);
+            }
         });
 
-        Button btnHybrid = (Button) findViewById(R.id.btnHybrid);
-        btnHybrid.setOnClickListener((v) -> {
-            if(mapReady)
-                mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        Button btnDublin = (Button) findViewById(R.id.btnDublin);
+        btnDublin.setOnClickListener((v) -> {
+            if(mapReady){
+                LatLng dublin = new LatLng(53.3478, 6.2597);
+                CameraPosition cp = CameraPosition.builder().target(dublin).zoom(14).bearing((float) 112.5).tilt(65).build();
+                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cp), 5000, null);
+            }
         });
 
         MapFragment mapFragment = (MapFragment) getFragmentManager()
@@ -49,7 +58,8 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         mapReady = true;
         mMap = googleMap;
         LatLng newYork = new LatLng(40.7484, -73.9857);
-        CameraPosition target = CameraPosition.builder().target(newYork).zoom(17).tilt(65).build();
+        CameraPosition target = CameraPosition.builder().target(newYork).zoom(17).bearing((float) 112.5).tilt(65).build();
         mMap.moveCamera(CameraUpdateFactory.newCameraPosition(target));
+        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
     }
 }
