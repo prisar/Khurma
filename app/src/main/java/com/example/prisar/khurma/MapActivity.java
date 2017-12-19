33cd135c30a863a6277da6752b2040f3c1c106b6
prisar <pritimysarkar2010@gmail.com>
2017-12-19 14:19:59 +0530

    Make the camera move: update

--- app/src/main/java/com/example/prisar/khurma/MapActivity.java
+++ app/src/main/java/com/example/prisar/khurma/MapActivity.java
@@ -68,31 +68,38 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
         renton = new MarkerOptions()
                 .position(new LatLng(47.489805, -122.120502))
-                .title("Renton");
+                .title("Renton")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher));
 
         kirland = new MarkerOptions()
                 .position(new LatLng(47.7301986, -122.1768858))
-                .title("Kirland");
+                .title("Kirland")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher));
 
         everett = new MarkerOptions()
                 .position(new LatLng(47.978748, -122.202001))
-                .title("Everett");
+                .title("Everett")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher));
 
         lynnwood = new MarkerOptions()
                 .position(new LatLng(47.819533, -122.32288))
-                .title("Lynnwood");
+                .title("Lynnwood")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher));
 
         montlake = new MarkerOptions()
                 .position(new LatLng(47.7973733, -122.3281771))
-                .title("Montlake Terrace");
+                .title("Montlake Terrace")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher));
 
         kent = new MarkerOptions()
                 .position(new LatLng(47.385938, -122.258212))
-                .title("Kent Valley");
+                .title("Kent Valley")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher));
 
         showare = new MarkerOptions()
                 .position(new LatLng(47.38702, -122.23986))
-                .title("Showare Center");
+                .title("Showare Center")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher));
 
         MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.mapOutput);
